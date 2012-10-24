991d24f5280234284e39cc875bd353fc37a7b224
wvengen <wvengen@nikhef.nl>
2012-10-24 13:06:06 +0200

    retrieve food from service using rest api call

--- src/main/java/org/example/PlateClient.java
+++ src/main/java/org/example/PlateClient.java
@@ -1,7 +1,10 @@
 package org.example;
  
 import java.io.IOException;
-import java.util.Enumeration;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONArray;
 
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
  
@@ -19,7 +25,11 @@ public class PlateClient extends HttpServlet
     @RequestMapping(value="/hello")
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        servePlate(response, new String[]{"empty shell"});
+    	try {
+            servePlate(response, readFood(new URI(apiurl)));    		
+    	} catch(URISyntaxException e) {
+    		throw new ServletException(e);
+    	}
     }
     
     @RequestMapping(value="/secure")
@@ -27,7 +37,19 @@ public class PlateClient extends HttpServlet
     {
         servePlate(response, new String[]{"empty shells"});
     }
+    
+    // return array from json returned by given url
+    private String[] readFood(URI url) throws IOException
+    {
+    	ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(url, HttpMethod.GET);
+    	request.getHeaders().add("Accept", "application/json");
+    	InputStream is = request.execute().getBody();
+    	StringBuffer data = new StringBuffer();
+    	while (is.available()>0) data.append((char)is.read());
+    	return (String[])JSONArray.fromObject(data.toString()).toArray(new String[0]);
+    }
 
+    // output html
     private void servePlate(HttpServletResponse response, String[] what) throws IOException
     {
         response.setStatus(HttpServletResponse.SC_OK);
@@ -44,5 +66,5 @@ public class PlateClient extends HttpServlet
     private String apiurl = null;
     @Required public void setApiurl(String a) { apiurl = a; }
     private String apiurls = null;
-    @Required public void setApiurls(String a) { apiurls = a; }        
+    @Required public void setApiurls(String a) { apiurls = a; }
 }
